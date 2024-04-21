bff15abe0708478e3274751e3dd88fff210d5f51
WillFerra <91874103+WillFerra@users.noreply.github.com>
2024-04-21 15:57:44 +0200

    Functions
    Added CRUD Functions for Bank table

--- core/bank.php
+++ core/bank.php
@@ -10,4 +10,135 @@ class Bank{
     public function __construct($db){
         $this->conn = $db;
     }
+
+    // Getting All Bank from database
+    public function readBanks(){
+
+        $query = 'SELECT * FROM ' .$this->table. ' s ORDER BY s.id ASC;';
+
+        // prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    // Getting Single bank from database by Id
+    public function getBankById(){
+
+        // Read Query
+        $query = 'SELECT * FROM ' .$this->table. ' WHERE id = ? LIMIT 1;';
+
+        // prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        // Bind the parameter
+        $stmt->bindParam(1,$this->id);
+
+        // execute query
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        if($stmt->execute()){
+            if($row==null){
+                printf('No record found');
+                return false;
+            }
+            $this->id = $row['id'];
+            $this->name = $row['name'];
+            return $stmt;
+        }
+
+        printf('Error: %s. \n', $stmt->error);
+        return false;
+    }
+
+    // Getting Single Bank from database by Name
+    public function getBankByName(){
+
+        // Read Query
+        $query = "SELECT * FROM " .$this->table. " 
+                WHERE name like ?;";
+
+        // prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        // Bind the parameter
+        $name = '%' . $this->name . '%';
+        $stmt->bindParam(1,$name);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    // Updating Bank by the ID
+    public function updateBank(){
+        $query = 'UPDATE '.$this->table.'
+                    SET name = :name
+                    WHERE id = :id;';
+        
+        $stmt = $this->conn->prepare($query);
+
+        // clean data sent by user (for security)
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->name = htmlspecialchars(strip_tags($this->name));
+
+        // bind parameters to request
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':name', $this->name);
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        printf('Error: %s. \n', $stmt->error);
+        return false;
+    }
+
+    // Creating New Bank
+    public function createBank(){
+        $query = 'INSERT INTO '.$this->table.'
+            (name)
+            VALUES(:name);';
+        
+        $stmt = $this->conn->prepare($query);
+
+        // clean data sent by user (for security)
+        $this->name = htmlspecialchars(strip_tags($this->name));
+
+        // bind parameters to request
+        $stmt->bindParam(':name', $this->name);
+
+        if ($stmt->execute()){
+            return true;
+        }
+
+        printf('Error $s. \n', $stmt->error);
+        return false;
+    }
+
+    // Deleting a Bank
+    public function deleteBank(){
+        $query = 'DELETE FROM '.$this->table.' WHERE id = :id;';
+        
+        $stmt = $this->conn->prepare($query);
+
+        // clean data sent by user (for security)
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // bind parameters to request
+        $stmt->bindParam(':id', $this->id);
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        printf('Error: %s. \n', $stmt->error);
+        return false;
+    }
 }
