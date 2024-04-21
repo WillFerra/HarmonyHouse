ff58c10df5fe080cab821136f2e7695f27cd91df
WillFerra <91874103+WillFerra@users.noreply.github.com>
2024-04-21 16:24:02 +0200

    Endpoints Added
    Added CRUD Endpoints for Section
    - Create Section
    - Get all sections
    - Get section by Id
    - Get Section by name
    - Update Section
    - Delete Section

---
+++ api/deleteSection.php
@@ -0,0 +1,24 @@
+<?php
+
+// Set endpoint headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+header('Access-Control-Allow-Methods: PATCH');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+
+// initialize API
+include_once('../core/initialize.php');
+
+// Create an instance of Section
+$section = new Section($db);
+
+$section->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+if($section->deleteSection()){
+    echo json_encode(array('message' => 'Section deleted.'));
+}
+else{
+    echo json_encode(array('message' => 'Section not deleted.'));
+}
--- core/section.php
+++ core/section.php
@@ -10,4 +10,134 @@ class Section{
     public function __construct($db){
         $this->conn = $db;
     }
+
+    // Getting All Sections from database
+    public function readSections(){
+
+        $query = 'SELECT * FROM ' .$this->table. ' s ORDER BY s.id ASC;';
+
+        // prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    // Getting Single Section from database by Id
+    public function getSectionById(){
+
+        // Read Query
+        $query = 'SELECT * FROM ' .$this->table. ' WHERE id = ? LIMIT 1;';
+
+        // prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        // Bind the parameter
+        $stmt->bindParam(1,$this->id);
+
+        // execute query
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        if($stmt->execute()){
+            if($row==null){
+                printf('No record found');
+                return false;
+            }
+            $this->id = $row['id'];
+            $this->name = $row['name'];
+            return $stmt;
+        }
+
+        printf('Error: %s. \n', $stmt->error);
+        return false;
+    }
+
+    // Getting Single Section from database by Name
+    public function getSectionByName(){
+
+        // Read Query
+        $query = "SELECT * FROM " .$this->table. " 
+                WHERE name like ?;";
+
+        // prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        // Bind the parameter
+        $name = '%' . $this->name . '%';
+        $stmt->bindParam(1,$name);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    // Updating Section by the ID
+    public function updateSection(){
+        $query = 'UPDATE '.$this->table.'
+                    SET name = :name
+                    WHERE id = :id;';
+        
+        $stmt = $this->conn->prepare($query);
+
+        // clean data sent by user (for security)
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->name = htmlspecialchars(strip_tags($this->name));
+
+        // bind parameters to request
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':name', $this->name);
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        printf('Error: %s. \n', $stmt->error);
+        return false;
+    }
+
+    // Creating New Section
+    public function createSection(){
+        $query = 'INSERT INTO '.$this->table.'
+            (name)
+            VALUES(:name);';
+        
+        $stmt = $this->conn->prepare($query);
+
+        // clean data sent by user (for security)
+        $this->name = htmlspecialchars(strip_tags($this->name));
+
+        // bind parameters to request
+        $stmt->bindParam(':name', $this->name);
+
+        if ($stmt->execute()){
+            return true;
+        }
+
+        printf('Error $s. \n', $stmt->error);
+        return false;
+    }
+
+    public function deleteSection(){
+        $query = 'DELETE FROM '.$this->table.' WHERE id = :id;';
+        
+        $stmt = $this->conn->prepare($query);
+
+        // clean data sent by user (for security)
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // bind parameters to request
+        $stmt->bindParam(':id', $this->id);
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        printf('Error: %s. \n', $stmt->error);
+        return false;
+    }
 }
