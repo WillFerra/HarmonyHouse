afd5e98210a0453aa41b2d8648ee8a750acce796
WillFerra <91874103+WillFerra@users.noreply.github.com>
2024-04-21 16:40:57 +0200

    Endpoints For Coutnry
    Added the Endpoints for Country table
    - Create country
    - Update Country
    - Delete Country
    - Get all Countries
    - Get Countries by id
    - Get Countries by name

--- core/country.php
+++ core/country.php
@@ -10,4 +10,135 @@ class Country{
     public function __construct($db){
         $this->conn = $db;
     }
+
+    // Getting All Countries from database
+    public function readCountries(){
+
+        $query = 'SELECT * FROM ' .$this->table. ' c ORDER BY c.id ASC;';
+
+        // prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    // Getting Single Country from database by Id
+    public function getCountryById(){
+
+        // Read Query
+        $query = 'SELECT * FROM ' .$this->table. ' WHERE id = ? LIMIT 1;';
+
+        // prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        // Bind the parameter
+        $stmt->bindParam(1,$this->id);
+
+        // execute query
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        if($stmt->execute()){
+            if($row==null){
+                printf('No record found');
+                return false;
+            }
+            $this->id = $row['id'];
+            $this->name = $row['name'];
+            return $stmt;
+        }
+
+        printf('Error: %s. \n', $stmt->error);
+        return false;
+    }
+
+    // Getting Single Country from database by Name
+    public function getCountryByName(){
+
+        // Read Query
+        $query = "SELECT * FROM " .$this->table. " 
+                WHERE name like ?;";
+
+        // prepare Statement
+        $stmt = $this->conn->prepare($query);
+
+        // Bind the parameter
+        $name = '%' . $this->name . '%';
+        $stmt->bindParam(1,$name);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    // Updating Country by the ID
+    public function updateCountry(){
+        $query = 'UPDATE '.$this->table.'
+                    SET name = :name
+                    WHERE id = :id;';
+        
+        $stmt = $this->conn->prepare($query);
+
+        // clean data sent by user (for security)
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->name = htmlspecialchars(strip_tags($this->name));
+
+        // bind parameters to request
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':name', $this->name);
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        printf('Error: %s. \n', $stmt->error);
+        return false;
+    }
+
+    // Creating New Country
+    public function createCountry(){
+        $query = 'INSERT INTO '.$this->table.'
+            (name)
+            VALUES(:name);';
+        
+        $stmt = $this->conn->prepare($query);
+
+        // clean data sent by user (for security)
+        $this->name = htmlspecialchars(strip_tags($this->name));
+
+        // bind parameters to request
+        $stmt->bindParam(':name', $this->name);
+
+        if ($stmt->execute()){
+            return true;
+        }
+
+        printf('Error $s. \n', $stmt->error);
+        return false;
+    }
+
+    // Deleting a Country
+    public function deleteCountry(){
+        $query = 'DELETE FROM '.$this->table.' WHERE id = :id;';
+        
+        $stmt = $this->conn->prepare($query);
+
+        // clean data sent by user (for security)
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // bind parameters to request
+        $stmt->bindParam(':id', $this->id);
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        printf('Error: %s. \n', $stmt->error);
+        return false;
+    }
 }
